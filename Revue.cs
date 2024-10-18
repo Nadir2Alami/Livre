2b46c753d80f2c47105f12c2f7e706c1ddcf4ebd
nadir <nadirlomi@yahoo.com>
2024-10-17 20:17:07 -0400

    SousCLasse_Revue

---
+++ Revue.cs
@@ -0,0 +1,28 @@
+﻿namespace AppConsoleLivres
+{
+    public class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+
+        public Revue(string titre, string auteur, int nombrePages, int numero, int annee) : base(titre, nombrePages,auteur) 
+        {
+            Numero = numero;
+            Annee = annee;
+
+
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur : {Auteur}, Nombredepages : {NombrePages}, Numero : {Numero} , Anee : {Annee}");
+
+        }
+
+
+
+
+
+
+    }
+}
