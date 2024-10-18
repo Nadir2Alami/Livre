7ac23052b1bc7b3ba7256854da6145bc594f8721
nadir <nadirlomi@yahoo.com>
2024-10-17 20:23:31 -0400

    livre

--- Livres.cs
+++ Livres.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppConsoleLivres
+﻿namespace AppConsoleLivres
 {
     public class Livre
     {
-        public string Titre {  get; set; }
+        public string Titre { get; set; }
         public string Auteur { get; set; }
         public int NombrePages { get; set; }
 
@@ -20,7 +14,7 @@ namespace AppConsoleLivres
             Auteur = auteur;
 
         }
-        public void AfficherDetails()
+        public virtual void AfficherDetails()
         {
             Console.WriteLine($"Titre:{Titre},Auteur : {Auteur}, Nombredepages : {NombrePages}");
         }
